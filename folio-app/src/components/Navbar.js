85376b87da236bbf9c7283f9fda3853938428a70
chilimango <maldonadot55@yahoo.com>
2020-11-20 15:24:19 -0800

    added navbar

--- folio-app/src/components/Navbar.js
+++ folio-app/src/components/Navbar.js
@@ -4,23 +4,23 @@ import React from "react";
 function Navbar() {
     return (
         <div>
-            <nav class="navbar">
-                <div class="col-md-8 col-sm-12 col-lg-8">
-                    <a class="navbar-brand name-logo" href="./index.html">teresa maldonado</a>
+            <nav className="navbar">
+                <div className="col-md-8 col-sm-12 col-lg-8">
+                    <a className="navbar-brand name-logo" href="./index.html">teresa maldonado</a>
                 </div>
-                <div class="col-md-8 col-sm-12 col-lg-4">
-                <ul class="navbar-nav" id="btn">
-                    <li class="nav-item">
-                        <a class="nav-link">
+                <div className="col-md-8 col-sm-12 col-lg-4">
+                <ul className="navbar-nav" id="btn">
+                    <li className="nav-item">
+                        <a className="nav-link">
             {/* <!-- Button trigger about modal --> */}
-                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#aboutModal">about</button>
-                        <span class="sr-only">(current)</span>
+                        <button type="button" className="btn btn-primary" data-toggle="modal" data-target="#aboutModal">about</button>
+                        <span className="sr-only">(current)</span>
                         </a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link">
+                    <li className="nav-item">
+                        <a className="nav-link">
             {/* <!-- Button trigger contact modal --> */}
-                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#contactModal">contact</button>
+                        <button type="button" className="btn btn-primary" data-toggle="modal" data-target="#contactModal">contact</button>
                         <span class="sr-only">(current)</span>
                         </a>
                     </li>
