125aaf3a39ddb2c45e98cbe88ef728b18fe4949a
chilimango <maldonadot55@yahoo.com>
2020-11-20 18:51:39 -0800

    added hero folder

--- folio-app/src/components/Hero/Hero.js
+++ folio-app/src/components/Hero/Hero.js
@@ -1,10 +1,11 @@
 import React from "react";
+import "./index.css";
 
 function Hero() {
     return (
         <div>
             <div className="landing col-sm-12 col-md-12">
-                <img className="landing-img hero-image" alt="illustration of Teresa on computer coding" src="../public/images/hero.jpeg"/>
+                <img className="landing-img hero-image" alt="illustration of Teresa on computer coding" src=".../components/public/images/hero.jpeg"/>
                 <div className="text-center intro centered">
                 </div>
             </div>
