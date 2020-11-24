0aec5a387239f1a5263ff1dee76bab04969b2951
chilimango <maldonadot55@yahoo.com>
2020-11-23 19:58:58 -0800

    added hover effect

--- folio-app/src/App.js
+++ folio-app/src/App.js
@@ -5,6 +5,7 @@ import Hero from "./components/Hero/Hero";
 import Jumbo from "./components/Jumbo/Jumbo";
 import Projects from "./components/Projects/Projects";
 import Footer from "./components/Footer/Footer";
+import OnHover from "./components/Hero/index"
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -14,6 +15,7 @@ function App() {
       <Navbar />
       <Modal/>
       <Hero />
+      <OnHover />
       <Jumbo />
       <Projects />
       <Footer />
