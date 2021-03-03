66fde7a7265a376236a44de9b01a3472ea1e5216
chilimango <maldonadot55@yahoo.com>
2021-03-03 14:15:18 -0800

    center name in navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,13 +5,12 @@ import "./index.css"
 function Navbar() {
     return (
         <div>
-            <nav className="navbar">
-                <div className="col-md-8 col-sm-12 col-lg-8">
+            <nav className="navbar justify-content-center">
+                <div>
                     <h1>
-                    <a className="navbar-brand name-logo" href="./index.html">teresa maldonado</a>
+                        <a className="navbar-brand name-logo" href="./index.html">teresa maldonado</a>
                     </h1>
                 </div>
-                <div className="col-md-8 col-sm-12 col-lg-4"></div>
                 {/* <ul className="navbar-nav" id="btn">
                     <li className="nav-item">
                         <button type="button" className="btn btn-primary" data-toggle="modal" data-target="#aboutModal">about</button>
