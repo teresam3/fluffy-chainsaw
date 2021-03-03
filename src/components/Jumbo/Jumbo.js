390e34e3be818cd06a18242f6d4656de1add8fcf
chilimango <maldonadot55@yahoo.com>
2021-03-03 13:25:33 -0800

    styled language icons

--- src/components/Jumbo/Jumbo.js
+++ src/components/Jumbo/Jumbo.js
@@ -6,26 +6,26 @@ function Jumbo() {
         <div className="jumbotron">
             <div className="container-fluid">
                 <ul className="img">
-                    <li>
-                        <img alt="html-5 logo" src="./images/icons8-html-5-48.png"/>
+                    <li className="html">
+                        <img alt="html-5 logo" src="./images/icons8-html-5-48 (1).png"/>
                     </li>
-                    <li>
+                    <li className="java">
                         <img alt="java logo" src="./images/icons8-java-48.png"/>
                     </li>
-                    <li>
+                    <li className="js"> 
                         <img alt="javascript logo" src="./images/icons8-javascript-logo-100.png"/>
                     </li>
-                    <li>
+                    <li className="mysql">
                         <img alt="mysql logo" src="./images/icons8-mysql-logo-100.png"/>
                     </li>
-                    <li>
+                    <li className="post">
                         <img alt="postgres logo" src="./images/icons8-postgresql-100.png"/>
                     </li>
-                    <li>
+                    <li className="python">
                         <img alt="python logo" src="./images/icons8-python-52.png"/>
                     </li>
-                    <li>
-                        <img alt="react logo" src="./images/icons8-react-128.png"/>
+                    <li className="react">
+                        <img alt="react logo" src="./images/icons8-react-128 (2).png"/>
                     </li>
                 </ul>
             </div>
