fa6611a266785bd07b9abb55905f1c469792c1c2
chilimango <maldonadot55@yahoo.com>
2021-03-03 12:45:32 -0800

    added languages logos onto jumbtron

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
       <Navbar />
       <Modal/>
       <Hero />
-      <OnHover />
+      {/* <OnHover /> */}
       <Jumbo />
       <Projects />
       <Footer />
--- src/components/Jumbo/Jumbo.js
+++ src/components/Jumbo/Jumbo.js
@@ -5,9 +5,31 @@ function Jumbo() {
     return (
         <div className="jumbotron">
             <div className="container-fluid">
-                <h3 className="display-4"> </h3>
+                <ul className="img">
+                    <li>
+                        <img alt="html-5 logo" src="./images/icons8-html-5-48.png"/>
+                    </li>
+                    <li>
+                        <img alt="java logo" src="./images/icons8-java-48.png"/>
+                    </li>
+                    <li>
+                        <img alt="javascript logo" src="./images/icons8-javascript-logo-100.png"/>
+                    </li>
+                    <li>
+                        <img alt="mysql logo" src="./images/icons8-mysql-logo-100.png"/>
+                    </li>
+                    <li>
+                        <img alt="postgres logo" src="./images/icons8-postgresql-100.png"/>
+                    </li>
+                    <li>
+                        <img alt="python logo" src="./images/icons8-python-52.png"/>
+                    </li>
+                    <li>
+                        <img alt="react logo" src="./images/icons8-react-128.png"/>
+                    </li>
+                </ul>
             </div>
-        </div>
+        </div> 
     )
 }
 
