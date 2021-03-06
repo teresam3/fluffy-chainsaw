83d9645fd69f818a25606bff55a24beae9e5330e
chilimango <maldonadot55@yahoo.com>
2021-03-05 16:12:55 -0800

    added name logo

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -13,13 +13,13 @@ function Navbar() {
                     </li>
                     <li>
                         <h1>
-                            <a className="navbar-brand name-logo justify-content-center" href="./index.html">teresa maldonado</a>
+                            <a className="navbar-brand name-logo justify-content-center" href="./index.html"><img alt="teresa maldonado" src="https://github.com/teresam3/teresa-maldonado/blob/main/public/images/TeresaMaldonado2.png?raw=true"/></a>
                         </h1>
                     </li>
                     <li className="nav-item">
                         <button type="button" className="btn btn-primary about" data-toggle="modal" data-target="#contactModal">contact</button>
                         <span className="sr-only">(current)</span>
-                    </li>
+                    </li> 
                 </ul>
             </nav>
         </div>
